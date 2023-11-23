eb95aefe75ea8e8bb61730fc71b34658c9846381
Wajahat <96912547+Wajahat-AliKhan@users.noreply.github.com>
2023-11-23 20:36:32 +0900

    .py
    python file

---
+++ cnn_fashionmnist.py
@@ -0,0 +1,54 @@
+# imports
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.layers import Input, Conv2D, Dense, Flatten, Dropout
+from tensorflow.keras.models import Model
+
+# Load in the data
+fashion_mnist = tf.keras.datasets.fashion_mnist
+
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+print("x_train.shape:", x_train.shape)
+
+# the data is only 2D since its a grayscale image
+# convolution expects height x width x color
+x_train = np.expand_dims(x_train, -1)
+x_test = np.expand_dims(x_test, -1)
+print(x_train.shape)
+
+# number of classes
+K = len(set(y_train))
+print("number of classes:", K)
+
+# Build the model using the functional API
+i = Input(shape=x_train[0].shape)
+x = Conv2D(32, (3, 3), strides=2, activation='relu')(i)
+x = Conv2D(64, (3, 3), strides=2, activation='relu')(x)
+x = Conv2D(128, (3, 3), strides=2, activation='relu')(x)
+x = Flatten()(x)
+x = Dropout(0.2)(x)
+x = Dense(512, activation='relu')(x)
+x = Dropout(0.2)(x)
+x = Dense(K, activation='softmax')(x)
+
+model = Model(i, x)
+
+# Compile and fit
+# Note: make sure you are using the GPU for this!
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+r = model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=15)
+
+# Plot loss per iteration
+import matplotlib.pyplot as plt
+plt.plot(r.history['loss'], label='loss')
+plt.plot(r.history['val_loss'], label='val_loss')
+plt.legend()
+
+# Plot accuracy per iteration
+plt.plot(r.history['accuracy'], label='acc')
+plt.plot(r.history['val_accuracy'], label='val_acc')
+plt.legend()
